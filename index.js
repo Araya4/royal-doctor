2b7dc53f4088acdf10fb272c165477195ae39bd8
Araya4 <araya.wut@gmail.com>
2024-09-01 01:04:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const content = document.getElementById('myVideo');
-const arrSrc = ["about:blank",
-"k67GBsH6KHMt0sBlZam" , 'k4vik4QaAhibmHBlZio' , 'k38EjQ3yvi40m6BlZim' , 'k2nISFY0nl4OY6BlZik','kZyrYnOwlYFoiNBlZig',
-'k6SMmcKlfLs5H6BlZii','k1zMQmiZCHqCMBBlZEi','k7q3j3t4Vvbs0WBlZEg','ko9txT3mPXqW44BlZEa','k5roxIbt2Tn5YRBlZEc',
-'k5jZQHHPOavmazBn4yG','kFznqQYbMplvFYBn4yy','kUkWgOFiaBqrm6Bn4tq','k5QsQYegtqJmBtBn4yC','k5B21GO2rEIMyGBn4yA',
-'k43avMNSRrAf7RBn4ys','k1psNf0L5VCUjSBn4yw','k5x7TlqgPAft7OBn4yu','k7JRMfE37Zw6HQBn4yq','k70FbxeovO1XJUBn4yE','1003029247','1003032131'];
-
-function handleContent(thisId){
-    if(thisId===0){
-        content.src = arrSrc[0];
-    }
-    else if(thisId>20){
-        content.src = `https://player.vimeo.com/video/${arrSrc[thisId]}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`;
+const arrSrc = ["k67GBsH6KHMt0sBlZam" , 'k4vik4QaAhibmHBlZio' , 'k38EjQ3yvi40m6BlZim' , 'k2nISFY0nl4OY6BlZik','kZyrYnOwlYFoiNBlZig',
+    'k6SMmcKlfLs5H6BlZii','k1zMQmiZCHqCMBBlZEi','k7q3j3t4Vvbs0WBlZEg','ko9txT3mPXqW44BlZEa','k5roxIbt2Tn5YRBlZEc',
+    'k5jZQHHPOavmazBn4yG','kFznqQYbMplvFYBn4yy','kUkWgOFiaBqrm6Bn4tq','k5QsQYegtqJmBtBn4yC','k5B21GO2rEIMyGBn4yA',
+    'k43avMNSRrAf7RBn4ys','k1psNf0L5VCUjSBn4yw','k5x7TlqgPAft7OBn4yu','k7JRMfE37Zw6HQBn4yq','k70FbxeovO1XJUBn4yE',
+    '1003029247','1003032131'];
+const videoUrl = arrSrc.map((v,idx)=>{
+    if(v==="about:blank"){
+        return "about:blank";
+    }else if(Number(idx)>19){
+        return `https://player.vimeo.com/video/${arrSrc[Number(idx)]}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`
     }else{
-        content.src = `https://www.dailymotion.com/embed/video/${arrSrc[thisId]}?autoplay=1`;
+        return `https://www.dailymotion.com/embed/video/${arrSrc[Number(idx)]}`
     }
-}
-var liS = document.querySelectorAll("li");
-        liS.forEach((element,idx)=>{
-            element.addEventListener('click',function(e){
-                e.preventDefault();
-                handleContent(idx+1);
-            })
-        })
+})
+
+
+
+const res = document.getElementById("myVideo");
+const textS = document.querySelectorAll(".text-box");
+textS.forEach((element,idx)=>{
+    element.addEventListener('click',function(e){
+        e.preventDefault();
+        res.src = videoUrl[Number(idx)];
+
+    })
+    
+})
